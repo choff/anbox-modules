342453adbddafeca473dd7eb889bdadeaa3c359b
PEKKA Chang <github.relation928@passinbox.com>
2026-07-05 18:00:48 +0800

    Fix: remove the redundant header inclusion

--- binder/deps.h
+++ binder/deps.h
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include <linux/ipc_namespace.h>
-#include <linux/mm.h>
 
 struct ipc_namespace* get_init_ipc_ns_ptr(void);
 void zap_page_range_single(struct vm_area_struct *vma, unsigned long address,
